9aa34be55225d276fb20dc312762640530d0d967
putrifus <38694925+putrifus@users.noreply.github.com>
2018-05-24 11:16:01 +0200

    toto

--- chkForm/insert_user.php
+++ chkForm/insert_user.php
@@ -1,10 +1,4 @@
 <?php
 // parse données d'inscription
-if(isset($_POST['submit'])){
     include ("../includes/insert_user.php");
-} else {
-    header("Location : ../index.php?post=missing");
-    die;
-}
-
 ?>
