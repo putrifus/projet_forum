e88e7d2107a5b509cd3c703d326c4e4134d6c24e
AxelLedieu <axel62860@gmail.com>
2018-05-24 13:41:30 +0200

    commit

--- pages/inscr.php
+++ pages/inscr.php
@@ -39,18 +39,21 @@
         <i class="fa fa-bars"></i>
       </button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
+      <ul class="navbar-nav ml-auto">
           <li class="nav-item mx-0 mx-lg-1">
-            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio">Accueil</a>
+            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="index.html">Accueil</a>
           </li>
           <li class="nav-item mx-0 mx-lg-1">
-            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about">Questionnaire</a>
+            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="questionnaire.html">Questionnaire</a>
           </li>
           <li class="nav-item mx-0 mx-lg-1">
-            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Classement</a>
+            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="classement.html">Classement</a>
           </li>
           <li class="nav-item mx-0 mx-lg-1">
-            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Liens</a>
+            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="pub.html">Liens</a>
+          </li>
+          <li class="nav-item mx-0 mx-lg-1">
+            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="seconnecter.html">Se connecter</a>
           </li>
         </ul>
       </div>
@@ -141,6 +144,50 @@
       </div>
     </div>
   </section>
+
+  <!-- Footer -->
+  <!-- A copier coller en bas de chaque page une fois fini -->
+  <footer class="footer text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 caseFooter">
+                    <img class="img-fluid mb-5 d-block mx-auto" src="img/adefi_final.png" alt="">
+                    <p class="lead mb-0">Description ADEFI <br>
+                        <a href="">lien ADEFI</a>.</p>
+                </div>
+                <div class="col-md-4 mb-5 mb-lg-0 caseFooter">
+                    <h4 class="text-uppercase mb-4">Suivre l'AFPA</h4>
+                    <ul class="list-inline mb-0">
+                        <li class="list-inline-item">
+                            <a class="btn btn-outline-light btn-social text-center rounded-circle" href="https://www.facebook.com/AFPA.JEUNES">
+                                <i class="fa fa-fw fa-facebook"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a class="btn btn-outline-light btn-social text-center rounded-circle" href="https://twitter.com/afpa_formation">
+                                <i class="fa fa-fw fa-twitter"></i>
+                            </a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a class="btn btn-outline-light btn-social text-center rounded-circle" href="https://www.linkedin.com/company/afpa">
+                                <i class="fa fa-fw fa-linkedin"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-4 mb-5 mb-lg-0 caseFooter">
+                    <h4 class="text-uppercase mb-4">AFPA Arras</h4>
+                    <p class="lead mb-0">1 Rue Copernic
+                        <br>62000 Arras</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <div class="copyright py-4 text-center text-white">
+        <div class="container copyright">
+            <small>Copyright &copy; CrewStiant</small>
+        </div>
+    </div>
 
   <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
   <div class="scroll-to-top d-lg-none position-fixed ">
