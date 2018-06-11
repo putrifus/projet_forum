e5ca54ee15115918b42c56ede1c3dd908a683edc
LilWhoreStier <yohann.demora@gmail.com>
2018-06-11 10:14:08 +0200

    Mise en forme de l'affichage du classement si égalité fin de top 10

--- includes/lien/classement.php
+++ includes/lien/classement.php
@@ -4,7 +4,8 @@ include ("../includes/fonctions.php");
 session_start();
 
 //récupère les résultats de la requete sur le score
-$res = top_classement();
+$res = classement();
+
 //retourne le code html du tableau de score
 $_SESSION['classement'] = printHightScore($res);
 
@@ -15,8 +16,6 @@ if(isset($_SESSION['quest'])){
 
 // si l'utilisateur est connecté récupère son classement
 if(isset($_SESSION['login'])){
-    //récupère les résultats de la requete sur le classement par utilisateur
-    $res = classementByUser();
     //retourne le code html du classement user
     $_SESSION['classUser'] = printClassementByUser($res);
 }
